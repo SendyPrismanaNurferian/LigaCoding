d0b4dee3b64e3c7921728c44812c8f24034b7d80
M4$ $3ÑÐÝ <84496427+SendyPrismanaNurferian@users.noreply.github.com>
2022-08-20 21:45:55 +0700

    Add files via upload

---
+++ Liga_coding-main/apss.js
@@ -0,0 +1,10 @@
+alert("Selamat Datang di Web Le Champione");
+/*------------ Scroll Reveal Animation ------------ */
+
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: "60px",
+});
+
+sr.reveal("body", {delay: 200});
+sr.reveal("", {origin: "top", delay: 700});
